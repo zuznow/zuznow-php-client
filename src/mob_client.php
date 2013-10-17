95f007f60153234d7ff54f1007e8ffc4ba1c0c0e
zuznow <chen@zuznow.com>
2013-10-17 19:55:37 +0200

    Update mob_client.php

--- src/mob_client.php
+++ src/mob_client.php
@@ -1,23 +1,13 @@
 <?php 
 require_once 'config.php';
 
-$compatible = true;
-if(!@include_once("HTTP/Request2.php")) 
-{
-	print "HTTP/Request2.php is required";
-	$compatible = false;
-}
-
-$begin_time = microtime(true);
+require_once("HTTP/Request2.php");
 
 function MOB_Fetch_Cache($cache_key){
 	global $key;
 	global $domain_id;
 	global $server_url;
 	global $cache_ttl;
-	global $compatible;
-	
-	if (!$compatible) return;
 	
 	$my_url = $server_url."get_cached_data.php?key=".$key."&domain_id=".$domain_id."&cache_key=".$cache_key."&cache_ttl=".$cache_ttl."&user_agent=".rawurlencode(MOB_Get_UA());
 	$request = new HTTP_Request2($my_url, HTTP_Request2::METHOD_GET);
@@ -50,7 +40,6 @@ function MOB_Fetch_Anonynous_Cache(){
 		$str = MOB_Fetch_Cache(md5($url));
 		if ($str != "")
 		{
-			$str.= "\n<!--\nServed from Anonymous cache\nTotal time ".(microtime(true)-$begin_time)."\n -->";
 			print $str;
 			exit;
 		}
@@ -59,19 +48,10 @@ function MOB_Fetch_Anonynous_Cache(){
 
 function MOB_Filter($str) {
 	try	{	
-		global $begin_time;
 		global $api_key;
 		global $domain_id;
-		global $version;
 		global $cache_type;
-		global $cache_ttl;
 		global $server_url;
-		global $aync_response;
-		global $compatible;
-		
-		if (!$compatible) return $str;
-		
-		$filter_begin_time = microtime(true);
 		
 		$compressed_data = gzencode($str);
 		
@@ -106,22 +86,8 @@ function MOB_Filter($str) {
 				$request->addPostParameter('cache_key', md5($str));
 			}
 		}
-		if (!$server_config)
-		{
-			$request->addPostParameter('version', $version);
-			$request->addPostParameter('cache_ttl', $cache_ttl);
-		}
 		$request->addPostParameter('data', base64_encode($compressed_data));
 		
-		if (!$server_config)
-		{
-			$rules = realpath(dirname(__FILE__))."/"."rules.txt";
-			if (file_exists($rules))
-			{
-				$compressed_rules = gzcompress(file_get_contents($rules));
-				$request->addPostParameter('rules', $compressed_rules);
-			}
-		}
 		try
 		{
 			$response = $request->send();			
@@ -131,69 +97,34 @@ function MOB_Filter($str) {
 		}	
 		if (is_object($response) && 200 == $response->getStatus()) 
 		{	
-			$str = $response->getBody();
-			$str.= "\n<!-- \nTotal time ".(microtime(true)-$begin_time)."\n -->";
-			$str.= "\n<!-- \nFilter time ".(microtime(true)-$filter_begin_time)."\n -->";
-			return $str;
+			return $response->getBody();
 		}
 		else if (is_object($response) && 302 == $response->getStatus()) 
 		{	
-			$data_url = $response->getHeader("Location");
-			if (!$aync_response || ($_SERVER['REQUEST_METHOD'] !== 'GET'))
+			$data_url_base = $response->getHeader("Location");
+			$count = 1;
+			while($count < = 10)
 			{
-				$count = 1;
-				while(true)
+					$data_url = $data_url_base."&key=".$api_key."&domain_id=".$domain_id."&cache_ttl=".$cache_ttl."&user_agent=".rawurlencode(MOB_Get_UA());
+				$request = new HTTP_Request2($data_url, HTTP_Request2::METHOD_GET);
+				try
 				{
-					$data_url.="&key=".$key."&domain_id=".$domain_id."&cache_ttl=".$cache_ttl."&user_agent=".rawurlencode(MOB_Get_UA());
-					$request = new HTTP_Request2($data_url, HTTP_Request2::METHOD_GET);
-					try
-					{
-						$response = $request->send();
-						$count++;
-					}
-					catch (HTTP_Request2_ConnectionException $e)
-					{
-					}	
-					if (is_object($response) && 200 == $response->getStatus()) 
-					{	
-						$str = $response->getBody();
-						$str.= "\n<!-- \nTotal time ".(microtime(true)-$begin_time)."\n -->";
-						$str.= "\n<!-- \nFilter time ".(microtime(true)-$filter_begin_time)."\n -->";
-						return $str;
-						exit;
-					}
-					else if ((is_object($response) && 404 != $response->getStatus())) 
-					{
-						break;
-					}
+					$response = $request->send();
+					$count++;
 				}
-			}
-			else
-			{
-				$data_url = str_replace("http://","",$data_url);
-				$data_url = rawurlencode($data_url);
-				
-				$async_load_template = realpath(dirname(__FILE__))."/"."async_load_template.txt";
-				if (file_exists($async_load_template))
+				catch (HTTP_Request2_ConnectionException $e)
 				{
-					header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
-					header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-					header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-					header("Cache-Control: post-check=0, pre-check=0", false);
-					header("Pragma: no-cache");
-					$str = file_get_contents($async_load_template);
-					$str = str_replace("%%DATAURL%%",$data_url,$str);
+				}	
+				if (is_object($response) && 200 == $response->getStatus()) 
+				{	
+					return $response->getBody();
+					exit;
 				}
-				else			
+				else if ((is_object($response) && 404 != $response->getStatus())) 
 				{
-					$str = "<html><head></head><body>\n<script>var cache_url ='/MOB/get_cached_data.php?cache_data_location=";
-					$str.= $data_url;
-					$str.="';\n";
-					$str.="</script>\n</body></html>";
+					break;
 				}
-				$str.= "\n<!-- \nTotal time ".(microtime(true)-$begin_time)."\n -->";
-				$str.= "\n<!-- \nFilter time ".(microtime(true)-$filter_begin_time)."\n -->";
-				return $str;
+				sleep(1);
 			}
 		}
 		$str.= "\n<!-- \n".$response->getStatus()."\n".$response->getBody()."\n -->";
@@ -211,7 +142,14 @@ function MOB_Parse_Query($query) {
 	$params = array(); 
 	foreach ($queryParts as $param) { 
 		$item = explode('=', $param); 
+		if (isset($item[1]))
+		{
 		$params[$item[0]] = $item[1]; 
+	} 
+		else
+		{
+			$params[$item[0]] = ""; 
+		}
 	} 
 
 	return $params; 
@@ -226,18 +164,21 @@ function MOB_Get_UA()
 }
 function MOB_Is_Test()
 {
-	$url = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-	$url .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-	$parsed_url = parse_url($url);
-	$parsed_query = MOB_Parse_Query($parsed_url["query"]);
-	if (array_key_exists("mobtest", $parsed_query))
-	{
-		setcookie("mobtest","true");
-		return true;
-	}
 	if ($_COOKIE['mobtest'] != "")
 	{
 		return true;
+	}
+	$url = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+	$url .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	$parsed_url = parse_url($url);
+	if (isset($parsed_url["query"]))
+	{
+		$parsed_query = MOB_Parse_Query($parsed_url["query"]);
+		if (array_key_exists("mobtest", $parsed_query))
+		{
+			setcookie("mobtest","true");
+			return true;
+		}
 	}
 	return false;
 }
@@ -261,9 +202,6 @@ function MOB_Is_Supported()
 
 function MOB_Filter_Enable()
 {	
-	global $compatible;
-	
-	if (!$compatible) return;
 	$url = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 	$url .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 	if (!preg_match("/MOB/",$url))
